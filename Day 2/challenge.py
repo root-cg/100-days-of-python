676c03b56083e89c615308d191f4ed0a3186aafc
Yogesh Narayankar <yogeshrootcg@gmail.com>
2023-03-03 00:43:49 +0530

    first challenge

---
+++ Day 2/challenge.py
@@ -0,0 +1,13 @@
+print("Hello"[4])
+
+# num_check = len("sting")
+# type(num_check)
+
+#challenge DATA TYPES
+
+two_digit_num = input("Enter Your Number: ")
+
+converted_num = int(two_digit_num[0]) + int(two_digit_num[1])
+print(converted_num)
+
+
