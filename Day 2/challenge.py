794bbebac3f70a5ecb8d86cde341578937124500
Yogesh Narayankar <yogeshrootcg@gmail.com>
2023-03-03 00:58:09 +0530

    BMI Challenge

--- Day 2/challenge.py
+++ Day 2/challenge.py
@@ -5,9 +5,18 @@ print("Hello"[4])
 
 #challenge DATA TYPES
 
-two_digit_num = input("Enter Your Number: ")
+# two_digit_num = input("Enter Your Number: ")
 
-converted_num = int(two_digit_num[0]) + int(two_digit_num[1])
-print(converted_num)
+# converted_num = int(two_digit_num[0]) + int(two_digit_num[1])
+# print(converted_num)
+
+#BMI CALCULATOR 
 
 
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+
+
+BMI_calc = int(weight) /float(height) ** 2
+
+print(int(BMI_calc))
