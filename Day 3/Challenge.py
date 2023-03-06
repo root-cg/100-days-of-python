ae5adcb34c604c28f6099dfd7db307058ef82c7a
Yogesh Narayankar <yogeshrootcg@gmail.com>
2023-03-06 15:24:18 +0530

    rollarcoaster multiple if challenge

---
+++ Day 3/Challenge.py
@@ -0,0 +1,109 @@
+# print("Welcome to the rollercoaster!")
+# height = int(input("What is your height in cm ?\n" ))
+
+
+# if height > 120:
+#     print("You can ride roallercoaster!")
+# else:
+#     print("Sorry, You can't Ride roallercoaster!")
+
+#odd or Even
+
+
+# number = int(input("Which Number do you want to check ?\n"))
+
+# if number % 2 == 0:
+#     print("This is an even number.")
+# else:
+#     print("This is an odd number.")
+
+
+#Rollercoaster with nested conditions
+
+# print("Welcome to the rollercoaster!")
+# height = int(input("What is your height in cm ?\n" ))
+# age = int(input("what is your age?"))
+
+# if height > 120:
+#     print("You can ride roallercoaster!")
+#     if age <= 12:
+#         print("Please pay $5.")
+#     elif( age > 12 and age <= 18):
+#         print("Please pay $7.")
+#     else:
+#         print("Please pay $12.")
+# else:
+#     print("Sorry, You can't Ride roallercoaster!")
+
+
+
+#BMI 2.0
+
+
+# weight = float(input("enter your weight in m: \n"))
+# height = float(input("enter your height in m: \n"))
+
+# BMI_calc = round(weight / (height * height))
+
+# if BMI_calc <= 18.5:
+#     print(f"Your BMI is {BMI_calc}, you are underweight")
+# elif (BMI_calc > 18.5 and BMI_calc <= 25):
+#     print(f"Your BMI is {BMI_calc}, you have normal weight")
+# elif (BMI_calc > 25 and BMI_calc <= 30):
+#     print(f"Your BMI is {BMI_calc}, you are overweight")
+# elif (BMI_calc > 30 and BMI_calc <= 35):
+#     print(f"Your BMI is {BMI_calc}, you are obsee")
+# else:
+#     print(f"Your BMI is {BMI_calc}, you are clinically Obese Kindally seek Medical Attention!")
+
+
+
+#LEAP YEAR CHECKER
+
+
+# year = int(input("which year do you want to check? \n"))
+
+
+# if (year % 4 == 0):
+#     if (year % 100 ==0):
+#         if (year % 400==0):
+#             print("This is a leap year!")
+#         else:
+#             print("This is Not a leap year!")
+#     else:
+#         print("This is a leap year!")
+
+# else:
+#     print("This is NOT a leap year!")
+
+
+
+#Roller Coaster with Multiple if Conditons
+
+# print("Welcome to the rollercoaster!")
+# height = int(input("What is your height in cm ?\n" ))
+# age = int(input("what is your age?"))
+# bill=0
+
+# if height > 120:
+#     print("You can ride roallercoaster!")
+#     if age <= 12:
+#         bill=5
+#         print("Children pay $5.")
+#     elif( age > 12 and age <= 18):
+#         bill=7
+#         print("Youth pay $7.")
+#     else:
+#         bill=12
+#         print("Adult pay $12.")
+    
+#     want_pic = (input("Do you want click your Picture? It will cost additional $3  Y or N... \n"))
+#     if (want_pic.lower() =='y'):
+#         bill += 3
+    
+#     print(f"Your final amount is {bill}")
+
+# else:
+#     print("Sorry, You can't Ride roallercoaster!")
+
+
