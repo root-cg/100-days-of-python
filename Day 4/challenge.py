faafddf13590c7a596498aaf4ce043be02ca4add
Yogesh Narayankar <yogeshrootcg@gmail.com>
2023-03-13 16:46:06 +0530

    Challenge Banker Roulette

--- Day 4/challenge.py
+++ Day 4/challenge.py
@@ -1,10 +1,22 @@
 # coin Toss
+# import random
+
+# coin_toss = random.randint(0, 1)
+
+
+# if coin_toss == 1:
+#     print("Heads")
+# else:
+#     print("Tails")
+
+
+# Banker Roulette
 import random
 
-coin_toss = random.randint(0, 1)
+names_string = input("Give me everybody's names, seperated by a comma. :)")
+names = names_string.split(", ")
+payer = len(names)
+rand_name = random.randint(0, payer-1)
 
-
-if coin_toss == 1:
-    print("Heads")
-else:
-    print("Tails")
+final_payer  = names[rand_name]
+print(final_payer +" is going to buy the meal today!")
