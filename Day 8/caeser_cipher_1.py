440d37b408f1479f36ecf69772dfc528d3a6d900
Yogesh Narayankar <yogeshroot@gmail.com>
2023-09-05 18:43:31 +0530

    encrypt part 1

---
+++ Day 8/caeser_cipher_1.py
@@ -0,0 +1,39 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+#TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
+
+def encrypt(text, shift):
+
+    cipher_list = []
+
+    for i in range(len(text)):
+        text_in = text[i]       
+        cipher = alphabet.index(text_in)
+        shift_i = cipher + shift
+        if shift_i > 25:
+            element =  alphabet[shift_i - 26]  
+        else:
+            element = alphabet[shift_i]
+        cipher_list.append(element)
+    
+    out_cipher = "".join(cipher_list)
+    print(out_cipher)
+    #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.  
+    #e.g. 
+    #plain_text = "hello"
+    #shift = 5
+    #cipher_text = "mjqqt"
+    #print output: "The encoded text is mjqqt"
+
+    ##HINT: How do you get the index of an item in a list:
+    #https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
+
+    ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
+
+#TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message. 
+
+encrypt(text, shift)
